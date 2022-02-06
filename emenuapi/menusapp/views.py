622ad61d5bd1b0a600dd1294ea2a3b71e652d4be
Jakub Wronka <jakubwronka7@gmail.com>
2022-02-06 19:35:47 +0100

    Add ViewSets and routing for Recipe and Menu

--- emenuapi/menusapp/views.py
+++ emenuapi/menusapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Recipe, Menu
+from .serializers import RecipeSerializer, MenuSerializer
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+
+class MenuViewSet(viewsets.ModelViewSet):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
