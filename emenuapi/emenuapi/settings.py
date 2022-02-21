94d39df7cfb110a8766544270f102e4443be9fae
Jakub Wronka <jakubwronka7@gmail.com>
2022-02-21 19:21:25 +0100

    Sort and filter Menu data by parameters

--- emenuapi/emenuapi/settings.py
+++ emenuapi/emenuapi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'menusapp',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -127,5 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+     'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ),
 }
